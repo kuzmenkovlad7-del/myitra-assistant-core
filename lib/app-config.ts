2b2711f05c26562d34ed76fe5a684d19c4c80d95
kuzmenkovlad7-del <kuzmenkovlad7@gmail.com>
2025-11-25 21:13:35 +0200

    Updated app-config.ts

--- lib/app-config.ts
+++ lib/app-config.ts
@@ -1,13 +1,9 @@
 // lib/app-config.ts
 
-export const APP_NAME =
-  process.env.NEXT_PUBLIC_APP_NAME || "TurbotaAI"
+export const APP_NAME = "MyITRA"
+export const APP_DOMAIN = "turbotaai.com"
 
-export const APP_DOMAIN =
-  process.env.NEXT_PUBLIC_APP_DOMAIN || "turbotaai.com"
-
-export const APP_URL =
-  process.env.NEXT_PUBLIC_APP_URL || `https://${APP_DOMAIN}`
-
-export const SUPPORT_EMAIL =
+export const APP_SUPPORT_EMAIL =
   process.env.NEXT_PUBLIC_SUPPORT_EMAIL || `support@${APP_DOMAIN}`
+
+export const CONTACT_EMAIL = APP_SUPPORT_EMAIL
