baf1223906dde70e991920916580735490028a8d
kuzmenkovlad7-del <kuzmenkovlad7@gmail.com>
2025-11-26 22:54:27 +0200

    Updated app-config.ts

--- lib/app-config.ts
+++ lib/app-config.ts
@@ -1,11 +1,9 @@
 // lib/app-config.ts
-export const APP_NAME = "TurbotaAI"
 
-export const APP_SUPPORT_EMAIL = "support@turbotaai.com"
+export const APP_NAME = "MyITRA"
+export const APP_DOMAIN = "turbotaai.com"
 
-// если захочешь централизовать соцсети:
-export const APP_SOCIAL = {
-  instagram: "https://instagram.com/turbotaai",
-  facebook: "https://facebook.com/turbotaai",
-  github: "https://github.com/turbotaai",
-}
+export const APP_SUPPORT_EMAIL =
+  process.env.NEXT_PUBLIC_SUPPORT_EMAIL || `support@${APP_DOMAIN}`
+
+export const CONTACT_EMAIL = APP_SUPPORT_EMAIL
